6731b25f5640502eb7e365d6b71141aa4598fa64
KerollsAdel <kerolles.adel@mitchdesigns.com>
2025-07-22 17:05:00 +0300

    s

--- automation.py
+++ automation.py
@@ -6,13 +6,9 @@ from ABUAUF.Credentials import *
 from ABUAUF.Locators import *
 from selenium.webdriver.common.keys import Keys
 def run_automation(username, password):
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1080')
-    
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # Run in headless mode
+    driver = webdriver.Chrome(options=options)
 
     try:
         driver.get('https://abuauf.com')
