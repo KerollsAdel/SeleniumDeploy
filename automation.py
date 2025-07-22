8a7963cb163aaaddd8ca2c2adf31650c0958b273
KerollsAdel <kerolles.adel@mitchdesigns.com>
2025-07-22 16:54:31 +0300

    Dock

--- automation.py
+++ automation.py
@@ -7,7 +7,7 @@ from ABUAUF.Locators import *
 from selenium.webdriver.common.keys import Keys
 def run_automation(username, password):
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # Run in headless mode
+    #options.add_argument('--headless')  # Run in headless mode
     driver = webdriver.Chrome(options=options)
 
     try:
@@ -15,6 +15,8 @@ def run_automation(username, password):
         time.sleep(2)
 
         # Login
+        driver.find_element(By.XPATH,CLOSE_BUTTON).click()
+        driver.get('https://abuauf.com/login')
         driver.find_element(By.XPATH,EMAIL).send_keys(username)
         driver.find_element(By.XPATH, PASSWORD).send_keys(password)
         driver.find_element(By.XPATH, PASSWORD).send_keys(Keys.RETURN)
@@ -27,7 +29,7 @@ def run_automation(username, password):
         # Add additional automation steps here
 
         # Example: check for success message
-        success_msg = driver.find_element(By.XPATH, locators.success_message).text
+       # success_msg = driver.find_element(By.XPATH, locators.success_message).text
 
         return f"Automation completed successfully: {success_msg}"
     except Exception as e:
